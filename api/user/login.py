619e3cb8cd897fdcf118ba89da5ed5a89df38a87
Jeonghyeon <89664413+JeongHyeon-01@users.noreply.github.com>
2023-02-07 01:47:14 +0900

    [ADD] user create & export jwt

--- api/user/login.py
+++ api/user/login.py
@@ -1,29 +1,79 @@
-import requests
-from fastapi import Depends
+import requests, json, os
+from fastapi import Depends, HTTPException, status
 from core.database import get_db
-from db import users
-from schemas import users
+from models.users  import User
+from schemas import user_schemas
 from sqlalchemy.orm import Session
+from dotenv import load_dotenv
+from jose import jwt
+from datetime import datetime, timedelta
 
-HOST = "kapi.kakao.com"
+KAKAO_REST_API_KEY = os.getenv("KAKAO_REST_API_KEY")
+secretkey = os.getenv("SECRET_KEY")
+algorithm = os.getenv("ALGORITHM")
 
-def create_user(db : Session, user : users.UserCreate):
-    db_user = users.User(
-        username = user.username,
-        email = user.email,
-        gender = user.gender,
-        age_range = user.age_range,
-        birthday = user.birthday
-    )
+def connect_kakao_server(authCode):
+    url = "https://kauth.kakao.com/oauth/token"
+    header = {
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+    data = {
+        "code" : authCode,
+        "client_id" : KAKAO_REST_API_KEY,
+        "grant_type" : "authorization_code"
+        }
+    a = requests.post(url=url, headers=header,data=data)
+    res = a.json()
 
-    db.add(db_user)
-    db.commit()
-    db.refresh(db_user)
-    return 0
+    return res
+
+def create_access_token(data : dict, expires_delta : timedelta or None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow()+ expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp" : expire})
+    encoded_jwt = jwt.encode(to_encode, secretkey, algorithm)
+    return encoded_jwt
 
 
 def get_token_data(authCode):
-    url = 'https://oauth2.googleapis.com/tokeninfo?id_token='
-    response = requests.get(url+authCode)
+    res = connect_kakao_server(authCode)
+    
+    user_data = res['access_token']
+    token_type = res['token_type']
+    url = 'https://kapi.kakao.com/v2/user/me'
+    header = {
+        "Authorization" : token_type + " " + user_data
+    }
+    response = requests.get(url = url, headers=header)
     user_info = response.json()
-    return user_info
+    data = {
+        "username" : user_info['kakao_account']['profile']['nickname'],
+        "email" : user_info['kakao_account']['email'],
+        "gender" : user_info['kakao_account']['gender'],
+        "age_range" : user_info['kakao_account']['age_range'],
+        "birthday" : user_info['kakao_account']['birthday']
+    }
+    return data
+
+def create_user(db : Session, user : user_schemas.UserCreate):
+    if not db.query(User).filter(User.email == user.email).first():
+        db_user = User(
+            username = user.username,
+            email = user.email,
+            gender = user.gender,
+            age_range = user.age_range,
+            birthday = user.birthday
+        )
+        db.add(db_user)
+        db.commit()
+        db.refresh(db_user)
+        token = create_access_token(data=user.__dict__)
+        print(token)
+        return token
+    else:
+        token = create_access_token(data=user.__dict__)
+        print(token)
+        return token
--- routers/users.py
+++ routers/users.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter, Request, Response, Header, Depends
-from api.user.login import get_token_data, gretting_user
+from api.user.login import create_user,get_token_data
 from sqlalchemy.orm import Session
 from typing import Optional
-
+from core import database
+from schemas import user_schemas
 router = APIRouter(
     prefix="/users",
     tags=["users"],
@@ -10,7 +11,22 @@ router = APIRouter(
 
 @router.post("/login")
 async def LoginUser(
+    db : Session = Depends(database.get_db),
     authCode: Optional[str] = Header(None)
 ):
-    a = get_token_data(authCode)
-    return a
+    try:
+        print("1")
+        user_create = get_token_data(authCode)
+        user = user_schemas.UserCreate(
+            username = user_create["username"],
+            email = user_create["email"],
+            birthday = user_create["birthday"],
+            gender = user_create["gender"],
+            age_range = user_create["age_range"]
+        )
+        create_user(db = db, user = user)
+        return 0
+    
+    except Exception as e:
+        print(str(e))
+        return e
