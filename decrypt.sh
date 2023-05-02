9b20e391cf5cc7b874fcb1e19d17ffea3f4570fc
Jeonghyeon <89664413+JeongHyeon-01@users.noreply.github.com>
2023-05-02 17:39:36 +0900

    fix_auto

--- decrypt.sh
+++ decrypt.sh
@@ -5,4 +5,4 @@ FILENAME="start_dev.sh.enc"
 echo "Enter password:"
 read -s PASSWORD
 
-openssl enc -d -aes-256-cbc -in $FILENAME -out start_dev.sh -pass pass:$PASSWORD
+openssl aes-256-cbc -d -in start_dev.sh.enc -pass pass:$PASSWORD | bash
