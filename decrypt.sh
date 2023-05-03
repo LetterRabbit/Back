095d32dfd59bc32beef2d130b1f06d66393f5770
Jeonghyeon <89664413+JeongHyeon-01@users.noreply.github.com>
2023-05-03 13:37:27 +0900

    fix_logic

--- decrypt.sh
+++ decrypt.sh
@@ -2,7 +2,19 @@
 
 FILENAME="start_dev.sh.enc"
 
-echo "Enter password:"
+# Download and install OpenSSL version
+curl -O https://www.openssl.org/source/openssl-$OPENSSL_VERSION.tar.gz
+tar -xvzf openssl-$OPENSSL_VERSION.tar.gz
+cd openssl-$OPENSSL_VERSION
+./config
+make
+sudo make install
+
+cd ./Back
+echo "Enter the decryption password received from the administrator:"
 read -s PASSWORD
 
 openssl aes-256-cbc -d -in start_dev.sh.enc -pass pass:$PASSWORD | bash
+
+rm openssl-$OPENSSL_VERSION.tar.gz
+echo "설치 파일 삭제 완료"
